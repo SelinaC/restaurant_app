4c995b02c592926a27f347bfa4b16cc5a8175015
SelinaC <selina.chotai@gmail.com>
2013-12-18 19:31:12 +0000

    styled users index page, part styled restaurant and restaurants index pages

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -7,6 +7,26 @@ class Restaurant < ActiveRecord::Base
   has_many :reviews
 
   mount_uploader :image, RestaurantImageUploader
+
+  # def self.fhrs_image
+  #   case Restaurant.hygiene_rating
+  #     when 0
+  #       @fhrs_image = "/assets/fhrs_0_en-gb.jpg"
+  #     when 1
+  #       @fhrs_image = "/assets/fhrs_1_en-gb.jpg"
+  #     when 2
+  #       @fhrs_image = "/assets/fhrs_2_en-gb.jpg"
+  #     when 3
+  #       @fhrs_image = "/assets/fhrs_3_en-gb.jpg"
+  #     when 4
+  #       @fhrs_image = "/assets/fhrs_4_en-gb.jpg"
+  #     when 5
+  #       @fhrs_image = "/assets/fhrs_5_en-gb.jpg"
+  #     else
+  #       @fhrs_image = nil
+  #   end
+  # end
+
 end
 
 # :name, :address1, :address2, :town, :postcode, :phone, :cuisine, :email, :website, :image, :hygiene_rating, :hygiene_rating_date, :longitude, :latitude
